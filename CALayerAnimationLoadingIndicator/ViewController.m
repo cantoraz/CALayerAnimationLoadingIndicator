414bd5300f71e20d580b31ff05b2609a088670c7
Cantoraz Chou <cantoraz@gmail.com>
2016-07-11 22:33:44 +0800

    ADDED: Add a complex CALayer animation.

--- CALayerAnimationLoadingIndicator/ViewController.m
+++ CALayerAnimationLoadingIndicator/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 @property (nonatomic, weak) IBOutlet UIView* loadingView;
+@property (nonatomic, weak) IBOutlet UIView* complexLoadingView;
+
+@property (nonatomic, strong) CAShapeLayer* anotherOvalShapeLayer;
 @end
 
 @implementation ViewController
@@ -33,11 +36,27 @@
     ovalShapeLayer.lineCap = kCALineCapRound;
     
     [_loadingView.layer addSublayer:ovalShapeLayer];
+    
+    // Initialize ovalShapeLayer for complexLoadingView
+    _anotherOvalShapeLayer = [[CAShapeLayer alloc] init];
+    _anotherOvalShapeLayer.strokeColor = [UIColor whiteColor].CGColor;
+    _anotherOvalShapeLayer.fillColor = [UIColor clearColor].CGColor;
+    _anotherOvalShapeLayer.lineWidth = 7;
+    
+    CGFloat anotherOvalRadius = _complexLoadingView.bounds.size.height/2 * .8;
+    CGRect anotherOvalRect = CGRectMake(_complexLoadingView.bounds.size.width/2 - anotherOvalRadius,
+                                        _complexLoadingView.bounds.size.height/2 - anotherOvalRadius,
+                                        2*anotherOvalRadius, 2*anotherOvalRadius);
+    _anotherOvalShapeLayer.path = [UIBezierPath bezierPathWithOvalInRect:anotherOvalRect].CGPath;
+    _anotherOvalShapeLayer.lineCap = kCALineCapRound;
+    
+    [_complexLoadingView.layer addSublayer:_anotherOvalShapeLayer];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [self __caz__beginSimpleAnimation];
+    [self __caz__beginComplexAnimation];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -55,6 +74,24 @@
     rotate.toValue = @(2 * M_PI);
     rotate.repeatCount = HUGE;
     [_loadingView.layer addAnimation:rotate forKey:nil];
+}
+
+- (void)__caz__beginComplexAnimation
+{
+    CABasicAnimation* strokeStartAnimation = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
+    strokeStartAnimation.fromValue = @(-.5);
+    strokeStartAnimation.toValue = @1;
+    
+    CABasicAnimation* strokeEndAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    strokeEndAnimation.fromValue = @0;
+    strokeEndAnimation.toValue = @1;
+    
+    CAAnimationGroup* strokeAnimationGroup = [[CAAnimationGroup alloc] init];
+    strokeAnimationGroup.duration = 1.5;
+    strokeAnimationGroup.repeatCount = HUGE;
+    strokeAnimationGroup.animations = @[strokeStartAnimation, strokeEndAnimation];
+    
+    [_anotherOvalShapeLayer addAnimation:strokeAnimationGroup forKey:nil];
 }
 
 @end
